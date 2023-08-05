efed702e256d41775f51b126bc0124a07186e3a9
Aman Prajapati <aman.prajapati09@gmail.com>
2023-08-05 13:51:17 +0530

    initial commit

---
+++ CTS POD/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CTS POD
+//
+//  Created by Aman Prajapati on 8/5/23.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func loadView() {
+        super.loadView()
+        
+    }
+        
+    
+
+}
+
